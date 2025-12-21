4065564b94703a829788560c1e81577728825793
Noah Cashin <noahc959@icloud.com>
2025-12-21 16:50:17 -0500

    Update Lib!

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
-const THREADS: usize = 4; // ZPAQ Threads for compression itself.
-// Your total threads = TOTAL_THREADS;
-// THREADS should be TOTAL_THREADS / RAYON_NUM_THREADS.
-// For example on 4c4t CPU, RAYON_NUM_THREADS =2, THREADS=2.
-// For 6c12t CPU, RAYON_NUM_THREADS=4, THREADS=3. (or vice versa depending on workload)
-// It is important to tune this value for your CPU and workload.
-
 use rayon::prelude::*;
+
+use std::sync::OnceLock;
+
+static NUM_THREADS: OnceLock<usize> = OnceLock::new();
 
 /// ------- Base Compression Functions -------
 #[inline(always)]
@@ -13,21 +10,13 @@ pub fn get_compressed_size(path: &str, method: &str) -> u64 {
     // Convert Input file to Vec<u8>, and reference that (compress_size only takes &[u8] input), and pass method.
     // Will panic if file does not exist, so it must be prevalidated.
     zpaq_rs::compress_size(&std::fs::read(path).unwrap(), method).unwrap()
-
 }
 #[inline(always)]
-pub fn get_compressed_size_parallel(path: &str, method: &str) -> u64 {
+pub fn get_compressed_size_parallel(path: &str, method: &str, threads: usize) -> u64 {
     // Convert Input file to Vec<u8>, and reference that (compress_size only takes &[u8] input), and pass method.
     // Will panic if file does not exist, so it must be prevalidated.
-    zpaq_rs::compress_size_parallel(&std::fs::read(path).unwrap(), method, THREADS).unwrap()
-
+    zpaq_rs::compress_size_parallel(&std::fs::read(path).unwrap(), method, threads).unwrap()
 }
-
-pub fn get_compressed_size_archival(path: &str, method: &str) -> u64 {
-    // Only ideal for methods 1-5 in some cases where you want to count deduplication as compression.
-    zpaq_rs::zpaq_add_archive_size_file(path, method, THREADS).expect("compression failed")
-}
-
 
 #[inline(always)]
 pub fn get_bytes_from_paths(paths: &[&str]) -> Vec<Vec<u8>> {
@@ -37,53 +26,75 @@ pub fn get_bytes_from_paths(paths: &[&str]) -> Vec<Vec<u8>> {
         .collect()
 }
 
-
 /// ------- Bulk File Compression Functions -------
 #[inline(always)]
-pub fn get_sequential_compressed_sizes_from_sequential_paths(paths: &[&str], method: &str) -> Vec<u64> {
+pub fn get_sequential_compressed_sizes_from_sequential_paths(
+    paths: &[&str],
+    method: &str,
+) -> Vec<u64> {
     // This will, in parallel load all files into memory, THEN in parallel compress each one, each with one thread.
-    // Use when File IO is the bottleneck 
+    // Use when File IO is the bottleneck
     // Only uses ONE ZPAQ THREAD.
     // For VERY large n (relative to threads) with small files (relative to memory) this may be useful.
-    get_bytes_from_paths(paths).par_iter()
+    get_bytes_from_paths(paths)
+        .par_iter()
         .map(|data| zpaq_rs::compress_size(data, method).unwrap())
         .collect()
 }
 
-
 #[inline(always)]
-pub fn get_parallel_compressed_sizes_from_sequential_paths(paths: &[&str], method: &str) -> Vec<u64> {
+pub fn get_parallel_compressed_sizes_from_sequential_paths(
+    paths: &[&str],
+    method: &str,
+    threads: usize,
+) -> Vec<u64> {
     // This will, in parallel load all files into memory, THEN in parallel compress each one, with THREADS. (for each file, the thread count is THREADS)
     // Use when File IO is the bottleneck.
     // Balanced parallelization between RAYON_NUM_THREADS and ZPAQ `THREADS` const. For when total dataset will fit in memory.
-    get_bytes_from_paths(paths).par_iter()
-        .map(|data| zpaq_rs::compress_size_parallel(data, method, THREADS).unwrap())
+    get_bytes_from_paths(paths)
+        .par_iter()
+        .map(|data| zpaq_rs::compress_size_parallel(data, method, threads).unwrap())
         .collect()
 }
 
 #[inline(always)]
-pub fn get_sequential_compressed_sizes_from_parallel_paths(paths: &[&str], method: &str) -> Vec<u64> {
+pub fn get_sequential_compressed_sizes_from_parallel_paths(
+    paths: &[&str],
+    method: &str,
+) -> Vec<u64> {
     // This will, in parallel, for each file, read it from disk and compress it with one thread. (one file, one thread)
     // Use when File IO is not the bottleneck. Lower memory usage. (does not preload dataset)
     // Only uses ONE ZPAQ THREAD. For VERY large n(relative to threads) with large files(relative to memory) this may be useful.
-    paths.par_iter()
+    paths
+        .par_iter()
         .map(|path| get_compressed_size(path, method))
         .collect()
 }
 
 #[inline(always)]
-pub fn get_parallel_compressed_sizes_from_parallel_paths(paths: &[&str], method: &str) -> Vec<u64> {
+pub fn get_parallel_compressed_sizes_from_parallel_paths(
+    paths: &[&str],
+    method: &str,
+    threads: usize,
+) -> Vec<u64> {
     // This will, in parallel, for each file, read it from disk and compress it with THREADS. (for each file, the thread count is THREADS)
     // Use when File IO is not the bottleneck. Lower memory usage. (does not preload dataset)
     // For large n(relative to threads) with VERY large files(relative to memory) this may be useful.
     // This will reflect RAYON_NUM_THREADS and THREAD const values.
-    paths.par_iter()
-        .map(|path| get_compressed_size_parallel(path, method))
+    paths
+        .par_iter()
+        .map(|path| get_compressed_size_parallel(path, method, threads))
         .collect()
 }
 
+/// Optimizes parallelization
 #[inline(always)]
 pub fn get_compressed_sizes_from_paths(paths: &[&str], method: &str) -> Vec<u64> {
-    // Computes the optimal parallelization strategy based on file sizes, available memory, number of files, and available threads.
-    
+    let n: usize = paths.len();
+    let num_threads: usize = *NUM_THREADS.get_or_init(|| num_cpus::get());
+    if n < num_threads {
+        get_parallel_compressed_sizes_from_parallel_paths(paths, method, (num_threads + n - 1) / n)
+    } else {
+        get_sequential_compressed_sizes_from_parallel_paths(paths, method)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     //println!("{}", get_compressed_size("scompressme", "5"));
 
     let paths: Vec<&str> = vec!["compressme", "scompressme", "largebench"];
-    for i in 0..32 {
-        println!("{:?}", get_sequential_compressed_sizes_from_sequential_paths(&paths, "x4.3ci1"));
+    for _ in 0..32 {
+        println!("{:?}", get_compressed_sizes_from_paths(&paths, "x4.3ci1"));
     }
     //println!("{:?}", get_parallel_compressed_sizes_from_sequential_paths(&paths, "x4.3ci1"));
     //println!("{:?}", get_sequential_compressed_sizes_from_parallel_paths(&paths, "x4.3ci1"));
